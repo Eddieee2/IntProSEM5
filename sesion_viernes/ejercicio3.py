0123c7c362f9d12b1075607f2aebbc7fb4cf42d8
Eddieee2 <lescoe1234@gmail.com>
2025-04-25 08:21:29 -0600

    Dia de la semana

---
+++ sesion_viernes/ejercicio3.py
@@ -0,0 +1,19 @@
+
+numero = int(input("Ingrese el dia de la semana de forma numerica(1-7): "))
+
+if numero == 1:
+    print("Lunes")
+elif numero == 2:
+     print("Martes")
+elif numero == 3:
+        print("Miercoles")
+elif numero == 4:
+        print("Jueves")
+elif numero == 5:
+        print("Viernes")
+elif numero == 6:
+        print("Sabado")
+elif numero == 7:
+        print("Domingo")
+        
+        
